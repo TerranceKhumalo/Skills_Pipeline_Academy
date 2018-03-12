d045f44503c7b4af40facf1f20d9a540049104cf
TerranceKhumalo <khumaloterrance@gmail.com>
2018-03-12 15:11:03 +0200

    This is the updated project which contains progress report(PReport), user profile etc.

--- src/controllers/MainDashBoradController.java
+++ src/controllers/MainDashBoradController.java
@@ -1,38 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
-public class MainDashBoradController {
-	@FXML
-	private AnchorPane anchor;
 
-	@FXML
-	private JFXDrawer drawer;
+public class MainDashBoradController implements Initializable {
 
-	@FXML
-	private JFXButton homeBtn;
+    @FXML
+    private AnchorPane anchor;
+	
+    @FXML
+    private JFXButton homeBtn;
 
-	@FXML
-	private JFXButton assignmentBtn;
+    @FXML
+    private JFXButton assignmentBtn;
 
-	@FXML
-	private JFXButton progressBtn;
+    @FXML
+    private JFXButton progressreportBtn;
 
-	@FXML
-	private JFXButton calendarBtn;
+    @FXML
+    private JFXButton calendarBtn;
 
-	@FXML
-	private JFXButton courseBtn;
+    @FXML
+    private JFXButton courseMaterialBtn;
 
-	@FXML
-	private JFXButton discussionsBtn;
-
-	@FXML
-	private JFXHamburger hamburger;
-
-}
+    @FXML
+    private JFXButton discussionBtn;
+    
+    @FXML
+    private JFXHamburger hamburger;	
+    
+    @FXML
+    private JFXDrawer drawer;
+     
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    	
+    	drawer.setSidePane();
+    	HamburgerBackArrowBasicTransition resize = new HamburgerBackArrowBasicTransition(hamburger);
+    	resize.setRate(- 1);
+    	hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
+    		resize.setRate(resize.getRate() * - 1);
+    		resize.play();
+    		
+    		if(drawer.isShown())
+    			drawer.close();
+    	else 
+    		drawer.open();
+    			
+    		
+    	});
+    	
+    	
+    	
+    }
+ }
